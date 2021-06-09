e61ad61998475ab5ccf732c454c736e538c27805
yogesh-kapare-au19 <85472173+yogesh-kapare-au19@users.noreply.github.com>
2021-06-09 19:18:44 +0530

    Add files via upload

---
+++ day02.py
@@ -0,0 +1,7 @@
+name=input("Enter your name:")
+mobile=int(input("Enter your mobile number:"))
+age=int(input("Enter your age:"))
+print("Name=",name)
+print("Mobile=",mobile)
+print("Age=",age)
+print(type(name),type(mobile),type(age))
